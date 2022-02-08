28d80e5a450b3a7f87f7868ee917c846bdc1be74
Mike <migueligr2000@gmail.com>
2022-02-08 23:54:19 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {SplitButtonModule} from 'primeng/splitbutton';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SplitButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
