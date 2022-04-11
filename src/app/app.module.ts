df3a2c8e22b98807cc79ec9b713d9b083786c4f8
Mike <migueligr2000@gmail.com>
2022-04-12 00:19:00 +0200

    graph and Api proof of concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {DropdownModule} from 'primeng/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import {SplitButtonModule} from 'primeng/splitbutton';
 import {ButtonModule} from 'primeng/button';
 import { GlobeComponent } from './components/globe/globe.component';
 import {OrderListModule} from 'primeng/orderlist';
 import { GraphComponent } from './components/graph/graph.component';
+import {FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -19,6 +21,10 @@ import { GraphComponent } from './components/graph/graph.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    DropdownModule,
+    NgxChartsModule,
     AppRoutingModule,
     SplitButtonModule,
     ButtonModule,
