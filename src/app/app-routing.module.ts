5644ff9d01eeb097a24245942234ac32d570354b
Mike <migueligr2000@gmail.com>
2022-04-11 20:25:57 +0200

    graph component created + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GlobeComponent } from './components/globe/globe.component';
+import { GraphComponent } from './components/graph/graph.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'globe', component: GlobeComponent},
+  {path: 'graph', component: GraphComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import {SplitButtonModule} from 'primeng/splitbutton';
 import {ButtonModule} from 'primeng/button';
 import { GlobeComponent } from './components/globe/globe.component';
 import {OrderListModule} from 'primeng/orderlist';
+import { GraphComponent } from './components/graph/graph.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GlobeComponent,
+    GraphComponent,
   ],
   imports: [
     BrowserModule,
