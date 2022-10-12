87fb6930a49faa2ade7a6c8535b8d623bab29618
Mike <migueligr2000@gmail.com>
2022-10-12 16:47:01 +0200

    route changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { GlobeComponent } from './components/globe/globe.component';
 import { GraphComponent } from './components/graph/graph.component';
 
 const routes: Routes = [
-  {path: 'globe', component: GlobeComponent},
+  { path: '', component: GlobeComponent },
   {path: 'graph', component: GraphComponent}
 ];
 
